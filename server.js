129e44d34e6f704064d8a5c6ebfbe8a82ce59199
sgukz <passathorn.dev@gmail.com>
2022-11-29 15:11:47 +0700

    test call api reponse data 2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const axios = require("axios");
+const axios = require("axios");
 const moment = require("moment");
 const request = require("request");
 moment.locale("th");
